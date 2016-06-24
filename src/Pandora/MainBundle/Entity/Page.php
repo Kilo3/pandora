bca92570baaab5fcca85ee4a430ce0de70fc4b82
Kilo3 <cubeintocube@gmail.com>
2016-06-25 00:20:05 +0500

    locale changes to ru pages url blanks fix

--- src/Pandora/MainBundle/Entity/Page.php
+++ src/Pandora/MainBundle/Entity/Page.php
@@ -79,7 +79,7 @@ class Page
      */
     public function setUrl($url)
     {
-        $this->url = $url;
+        $this->url = str_replace(' ', '%20', $url);
 
         return $this;
     }
@@ -91,7 +91,7 @@ class Page
      */
     public function getUrl()
     {
-        return $this->url;
+        return $this->url = str_replace('%20', ' ', $this->url);
     }
 
     /**
